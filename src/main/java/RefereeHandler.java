fd3fe660ac57d1a76fb001d0f6a5d209832d99e2
Daria Smetanina <Daria_Smetanina@epam.com>
2021-10-15 16:28:08 +0300

    Referee functional

--- src/main/java/RefereeHandler.java
+++ src/main/java/RefereeHandler.java
@@ -1,21 +1,130 @@
 import sofia_kp.SSAP_sparql_response;
 import sofia_kp.iKPIC_subscribeHandler2;
+import wrapper.SmartSpaceException;
 import wrapper.SmartSpaceKPI;
+import wrapper.SmartSpaceTriple;
 
 import java.util.Vector;
 
 public class RefereeHandler implements iKPIC_subscribeHandler2 {
 
     SmartSpaceKPI kpi;
+    private Player player1;
+    private Player player2;
 
     RefereeHandler(SmartSpaceKPI kp) {
         kpi = kp;
+        player1 = new Player();
+        player2 = new Player();
     }
 
     @Override
     public void kpic_RDFEventHandler(Vector<Vector<String>> vector, Vector<Vector<String>> vector1, String s, String s1) {
         for (Vector<String> data : vector) {
+            /// сообщение о готовности, начало игры
+            // > user is ready
+            // < user serve game
+            if(data.get(1)=="is"){
+                if(!player1.isReady()){
+                    player1.setName(data.get(0));
+                    player1.setReady();
+                }
+                else{
+                    player2.setName(data.get(0));
+                    player2.setReady();
+                }
+                if(player1.isReady() && player2.isReady()){
+                    int randomNumber=((int)(Math.random() * (1)));
+                    String userName="";
+                    if(randomNumber==0){
+                        userName= player1.getName();
+                    }
+                    else{
+                        userName= player2.getName();
+                    }
+                    try {
+                        kpi.insert(new SmartSpaceTriple(userName,"start","game"));
+                    } catch (SmartSpaceException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            /// отслеживание игры, начисление баллов
+            // > user try_save point
+            // < user serve game
+            /// остановка игры 11+
+            // < user wins game
+            if(data.get(1)=="try_save"){
+                SmartSpaceTriple tri = watchGame();
+                try {
+                    kpi.remove(new SmartSpaceTriple(tri.getSubject(),"serve","game"));
+                    kpi.insert(tri);
+                } catch (SmartSpaceException e) {
+                    e.printStackTrace();
+                }
 
+            }
+        }
+    }
+
+    private SmartSpaceTriple watchGame(){
+        int randomNumber=((int)(Math.random() * (102)));
+        SmartSpaceTriple result;
+        String subj="";
+        String prdc="serve";
+        String obj="game";
+        if(randomNumber/2==1){
+            if(player1.increaseScore()){
+                subj=player2.getName();
+            }
+            else{
+                subj=player1.getName();
+                prdc="wins";
+                obj="game";
+            }
+        }
+        if(randomNumber/2==0){
+            if(player2.increaseScore()){
+                subj=player1.getName();
+            }
+            else{
+                subj=player2.getName();
+                prdc="wins";
+                obj="game";
+            }
+        }
+        return new SmartSpaceTriple(subj, prdc, obj);
+    }
+
+    private class Player{
+        private String name="";
+        private boolean ready=false;
+        private int score=0;
+
+        private String getName() {
+            return name;
+        }
+
+        private void setName(String name) {
+            this.name = name;
+        }
+
+        private boolean isReady() {
+            return ready;
+        }
+
+        private void setReady() {
+            this.ready = true;
+        }
+
+        private boolean increaseScore() {
+            this.score++;
+            if(score<11){
+                return true;
+            }
+            else{
+                return false;
+            }
         }
     }
 
@@ -33,4 +142,5 @@ public class RefereeHandler implements iKPIC_subscribeHandler2 {
     public void kpic_ExceptionEventHandler(Throwable throwable) {
 
     }
+
 }
